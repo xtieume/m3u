0592e7e36e5d4680c9197c2fa704a2f6797276d3
xtieume <107537167+xtieume@users.noreply.github.com>
2022-09-27 17:51:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 header("Content-Type: audio/mpegurl;");
-echo file_get_contents('i.m3u');
+if(isset($_GET['type']) && $_GET['type'] == 'epg'){
+  echo file_get_contents('http://onetv.click/schedule/epg.xml');
+}
+else{
+  echo file_get_contents('i.m3u');
+}
